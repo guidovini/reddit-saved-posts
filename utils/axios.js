a513c1cee108fd354309f940a6abd7181b2c8966
Guido Santillan <guidosantillan01@gmail.com>
2020-02-20 20:14:50 +0100

    Add axios.js

---
+++ utils/axios.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+// const getToken = require('./getToken');
+const { TOKEN, URL } = require('../constants');
+
+const axiosInstance = axios.create({
+  baseURL: URL,
+  headers: {
+    Authorization: `Bearer ${TOKEN}`
+  }
+});
+
+const axiosHelper = async (config) => {
+  try {
+    const response = await axiosInstance(config);
+    return response.data;
+  } catch (error) {
+    console.log('Error at axios.js');
+    // console.log(error);
+  }
+};
+
+module.exports = axiosHelper;
