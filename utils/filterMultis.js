95f2c207f86a473a1081f3199294d8e0e1a4cf65
Guido Santillan <guidosantillan01@gmail.com>
2020-02-20 20:15:38 +0100

    Add filterMultis.js

---
+++ utils/filterMultis.js
@@ -0,0 +1,13 @@
+const filterMultis = (multis = []) => {
+  let filteredMultis = [];
+  multis.map((multi) => {
+    filteredMultis.push({
+      name: multi.data.name,
+      subreddits: multi.data.subreddits.map((s) => s.name)
+    });
+  });
+  // console.log(filteredMultis);
+  return filteredMultis;
+};
+
+module.exports = filterMultis;
