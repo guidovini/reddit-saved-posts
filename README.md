1608495c30ec92cf118a9387984d90bc32277c60
Guido Santillan <guidosantillan01@gmail.com>
2020-02-20 20:13:51 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Reddit Saved posts
+
+## Instructions
+
+1. Get Token. Run:
+
+```ssh
+npm run get-token
+```
+
+2. Test waters first
---
+++ app.js
@@ -0,0 +1,22 @@
+const filterSubreddits = require('./utils/filterSubreddits');
+const getTotalPosts = require('./utils/getTotalPosts');
+const formatPosts = require('./utils/formatPosts');
+const saveToFile = require('./utils/saveToFile');
+const unsaveListPosts = require('./utils/unsaveListPosts');
+
+const categories = 'dev'; // ['dev', 'business', 'life']
+const batch = 4;
+const postsPerBatch = 20;
+
+const totalPosts = batch * postsPerBatch;
+// const totalPosts = 30;
+
+const startRoutine = async (totalPosts, categories, postsPerBatch) => {
+  const subreddits = filterSubreddits(categories);
+  const posts = await getTotalPosts(totalPosts, subreddits);
+  const formattedPosts = formatPosts(posts, postsPerBatch);
+  saveToFile(categories, formattedPosts);
+  // unsaveListPosts(posts);
+};
+
+startRoutine(totalPosts, categories, postsPerBatch);
