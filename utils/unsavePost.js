c44cdd2a85c34e579df560dec151bda0677e1197
Guido Santillan <guidosantillan01@gmail.com>
2020-02-20 20:18:23 +0100

    Add unsavePost.j

---
+++ utils/unsavePost.js
@@ -0,0 +1,11 @@
+const axiosHelper = require('./axios');
+
+const unsavePost = async (name) => {
+  await axiosHelper({
+    method: 'POST',
+    url: `/api/unsave?id=${name}`
+  });
+  return true;
+};
+
+module.exports = unsavePost;
