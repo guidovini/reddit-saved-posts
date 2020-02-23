63cc7f539191c39cd87c79519e2870263c7f7a75
Guido Santillan <guidosantillan01@gmail.com>
2020-02-24 00:08:16 +0100

    Display console messages

--- utils/getTotalPosts.js
+++ utils/getTotalPosts.js
@@ -5,19 +5,27 @@ const selectedPosts = [];
 
 const getTotalPosts = async (totalPosts = 80, subreddits = [], after = '') => {
   try {
+    console.log('     Fetching posts...');
     const posts = await fetchPosts({ after });
+
+    console.log('     Filtering posts by subreddit...');
     const [filteredPosts, next] = filterPosts(posts, subreddits);
+
     selectedPosts.push(...filteredPosts);
+    console.log('      ', selectedPosts.length);
 
     if (selectedPosts.length < totalPosts) {
       return getTotalPosts(totalPosts, subreddits, next);
     }
 
-    console.log('Posts processed: ', selectedPosts.length);
+    console.log('     Posts processed: ', selectedPosts.length);
     return selectedPosts;
   } catch (error) {
-    console.log('Error at getTotalPosts.js');
-    console.log('There are probably not enough posts for your request.');
+    console.log(
+      'Error at getTotalPosts.js: ',
+      'There are probably not enough posts for your request.'
+    );
+    console.log('      ', selectedPosts.length);
   }
 };
 
