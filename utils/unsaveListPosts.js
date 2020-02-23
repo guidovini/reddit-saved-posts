2bd48e293a5b2e5244eeb7dc1d3b81934f1ccd55
Guido Santillan <guidosantillan01@gmail.com>
2020-02-24 00:08:41 +0100

    Handle empty posts

--- utils/unsaveListPosts.js
+++ utils/unsaveListPosts.js
@@ -1,9 +1,12 @@
 const unsavePost = require('./unsavePost');
 
-const unsaveListPosts = (posts) => {
-  posts.map(({ name }) => unsavePost(name));
-  console.log('List of posts was unsaved');
-  return true;
+const unsaveListPosts = (posts = []) => {
+  if (posts.length > 0) {
+    posts.map(({ name }) => unsavePost(name));
+    console.log('List of posts was unsaved');
+  } else {
+    console.log('No posts found at unsaveListPosts.js');
+  }
 };
 
 module.exports = unsaveListPosts;
