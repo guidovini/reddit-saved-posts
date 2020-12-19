7b41c0d95da477144d0af3336be5a49fb3f5c034
Guido Santillan Arias <guidosantillan01@gmail.com>
2020-12-19 13:28:28 +0100

    fix tests

--- utils/__tests__/filterSubreddits.test.js
+++ utils/__tests__/filterSubreddits.test.js
@@ -14,22 +14,22 @@ const SCHEMA = [
 
 describe('filterSubreddits', () => {
   it('should filter correct subreddits', () => {
-    const subreddits = filterSubreddits({ categories, SCHEMA });
+    const subreddits = filterSubreddits(categories, SCHEMA);
     expect(subreddits).toEqual(['subr1', 'subr2', 'subr3']);
   });
 
   it('should not filter incorrect subreddits', () => {
-    const subreddits = filterSubreddits({ categories, SCHEMA });
+    const subreddits = filterSubreddits(categories, SCHEMA);
     expect(subreddits).not.toEqual(['b1', 'b2', 'b3']);
   });
 
   it('should return empty array when no SCHEMA was provided', () => {
-    const subreddits = filterSubreddits({ categories });
+    const subreddits = filterSubreddits(categories);
     expect(subreddits).toEqual([]);
   });
 
   it('should return empty array when no categories were provided', () => {
-    const subreddits = filterSubreddits({ SCHEMA });
+    const subreddits = filterSubreddits(SCHEMA);
     expect(subreddits).toEqual([]);
   });
 
